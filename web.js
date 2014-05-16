5bd3cc981d6d1fc25824bb56ee3020928c2a02db
goohara <carclassic1@gmail.com>
2014-05-16 17:49:20 +0700

    init

---
+++ web.js
@@ -0,0 +1,14 @@
+var express     = require('express');
+var logfmt      = require('logfmt');
+var app         = express();
+var port        = Number(process.env.PORT || 5000);
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+        res.send('Hello World');
+});
+
+app.listen(port, function() {
+        console.log('Listening on ' + port);
+})
