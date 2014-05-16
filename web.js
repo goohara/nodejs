6545174f85d85ace7104a74ce706edf8ba92e1d3
goohara <carclassic1@gmail.com>
2014-05-16 18:05:32 +0700

    Ebit

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var port        = Number(process.env.PORT || 5000);
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-        res.send('Hello World' + port);
+        res.send('Hello World : PORT ' + port);
 });
 
 app.listen(port, function() {
